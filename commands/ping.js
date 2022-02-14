0c06a49f873c779ebb95b9613125fd5f1278e798
흑곰 <58897994+heukkom@users.noreply.github.com>
2022-02-14 21:05:57 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed }  = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const ping = Date.now() - interaction.createdTimestamp;
+		const APIping = Math.round(interaction.client.ws.ping);
+
+		const embed = new MessageEmbed()
+			.setColor('#FFB6C1')
+			.setTitle('Pong!')
+			.setDescription(
+				'지연시간 `' +`${ping}` + ' ms`\n' +
+				'API지연시간 `' + `${APIping}` + 'ms`')
+			.setTimestamp()
+
+		await interaction.reply({ ephemeral: false, embeds: [embed] })
 	},
-};
+};	
