e02070d06694eac8c602e7acbd7c95843619f4af
흑곰 <58897994+heukkom@users.noreply.github.com>
2022-02-18 21:02:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,10 @@ const fs = require('fs');
 const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_WEBHOOKS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_VOICE_STATES ] });
 const { DiscordTogether } = require('discord-together');
 
 client.discordTogether = new DiscordTogether(client);
-
-client.on('messageCreate', async message => { // 'message' for Discord.js v12
-    if (message.content === ';유튜브') {
-        if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
-                return message.channel.send(`${invite.code}`);
-            });
-        };
-    };
-});
-
-client.on('messageCreate', async message => { // 'message' for Discord.js v12
-    if (message.content === ';포커') {
-        if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'poker').then(async invite => {
-                return message.channel.send(`${invite.code}`);
-            });
-        };
-    };
-});
-
-client.on('messageCreate', async message => { // 'message' for Discord.js v12
-    if (message.content === ';체스') {
-        if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'chess').then(async invite => {
-                return message.channel.send(`${invite.code}`);
-            });
-        };
-    };
-});
-
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -48,6 +17,11 @@ for (const file of commandFiles) {
 
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`ㅎㅇ`, {
+    type: "PLAYING",
+    url: "https://www.twitch.tv/wookwakgood"
+  });
 });
 
 client.on('shardError', error => {
@@ -69,21 +43,5 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
-client.on("messageCreate", message => {
-	const prefix = '!';
-    if (message.content.indexOf(prefix) !== 0) {return false};
-
-    const arguments = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = arguments.shift().toLowerCase();
-
-    if (cmd == "청소") {
-        if (!arguments[0]) return message.channel.send("2와 100사이 숫자로")
-        if (arguments[0] < 2 || arguments[0] > 100) return message.channel.send("2와 100사이 숫자로")
-
-        message.channel.bulkDelete(arguments[0]).then(messages => {
-            message.channel.send(`메세지 ${messages.size}개가 삭제됨`);
-        }).catch(e => console.log(e));
-    };
-});
 
 client.login(token);
