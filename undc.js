12465c4d82c67bfe72f1982eafdce027fc221e79
흑곰 <58897994+heukkom@users.noreply.github.com>
2022-02-18 21:24:04 +0900

    Create undc.js

---
+++ undc.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const token = 'OTI1Njc3NTc3MjQ1NDU0MzU2.YcwmbA.9pEi4h4ZHBSNE20ZJSpqKFGCOYA';
+const clientId = '925677577245454356';
+const guildId = '880826396887355433';
+    
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationGuildCommands(clientId, guildId))
+    .then(data => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+        }
+        return Promise.all(promises);
+    });
